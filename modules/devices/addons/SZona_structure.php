6ad1f734739fab36f4389c39c6183547572cf6de
Паша <odinwolk@gmail.com>
2022-01-15 12:42:24 +0300

    Update SZona_structure.php

--- modules/devices/addons/SZona_structure.php
+++ modules/devices/addons/SZona_structure.php
@@ -5,17 +5,6 @@ $this->device_types['zona'] = array(
     'PARENT_CLASS'=>'SControllers',
     'CLASS'=>'SZona',
     'PROPERTIES'=>array(
-            'loadType'=>array('DESCRIPTION'=>LANG_DEVICES_LOADTYPE,
-                '_CONFIG_TYPE'=>'select','_CONFIG_HELP'=>'SdLoadType',
-                '_CONFIG_OPTIONS'=>'light='.LANG_DEVICES_LOADTYPE_LIGHT.
-                    ',heating='.LANG_DEVICES_LOADTYPE_HEATING.
-                    ',vent='.LANG_DEVICES_LOADTYPE_VENT.
-                    ',curtains='.LANG_DEVICES_LOADTYPE_CURTAINS.
-                    ',gates='.LANG_DEVICES_LOADTYPE_GATES.
-                    ',power='.LANG_DEVICES_LOADTYPE_POWER.
-                    ',gates='.LANG_DEVICES_LOADTYPE_GATES.
-                    ',power='.LANG_DEVICES_LOADTYPE_POWER),
-            'icon'=>array('DESCRIPTION'=>LANG_IMAGE,'_CONFIG_TYPE'=>'style_image','_CONFIG_HELP'=>'SdIcon')
     ),
     'METHODS'=>array(
         'statusUpdated'=>array('DESCRIPTION'=>'Состояние Updated'),
